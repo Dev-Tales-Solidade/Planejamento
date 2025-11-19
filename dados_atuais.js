5af823b74a0cfb90add764aec6da95a981ae1c94
Dev-Tales-Solidade <tales1571998@gmail.com>
2025-11-19 08:21:05 -0300

    Update dados_atuais.js

--- dados_atuais.js
+++ dados_atuais.js
@@ -26,8 +26,8 @@ const MOCK_ESTOQUE_LIQUIDO = [
   {
     "Produto": "PAINEL RIPADO",
     "Perfil": "PRETO",
-    "M3_Estoque_Liquido": 24.582144476799982,
-    "Pecas_Estoque_Liquido": 6300.0
+    "M3_Estoque_Liquido": 52.441908176799984,
+    "Pecas_Estoque_Liquido": 13440.0
   }
 ];
 
